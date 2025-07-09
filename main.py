c3c6774bfa1a160f0e8a7cd45ed336d22b0ba79c
ZERO34802 <99256532+ZERO34802@users.noreply.github.com>
2025-07-09 23:18:23 +0530

    Added Exception Handling for characters more than 1000 length

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from routes.translation import router
+from fastapi.exceptions import RequestValidationError
+from utils.exception_handlers import custom_validation_exception_handler
+
 
 app = FastAPI(title="Translation Microservice")
+
+
+# Register the custom handler
+app.add_exception_handler(RequestValidationError,
+                          custom_validation_exception_handler)
 
 # Include the translation routes
 app.include_router(router)
 
 # Health check endpoint
+
+
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
---
+++ utils/exception_handlers.py
@@ -0,0 +1,20 @@
+from fastapi import Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.exception_handlers import request_validation_exception_handler
+
+
+async def custom_validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = exc.errors()
+    for err in errors:
+        if (
+            err["loc"] == ("body", "text")
+            and err["type"] == "value_error.any_str.max_length"
+        ):
+            return JSONResponse(
+                status_code=400,
+                content={"error": "Text too long. Max 1000 characters allowed."}
+            )
+
+    # Fallback for other validation errors
+    return await request_validation_exception_handler(request, exc)
